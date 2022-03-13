dab56d4c94613617cd75c9d1bea0621b038f8f65
nzzuhdi <nawfalzuhdi@gmail.com>
2022-03-13 20:12:33 +0700

    add l

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`listenixtzgianaaggk ${port}`);
+  console.log(`listenixtzgianaaggkl ${port}`);
 });
 
 module.exports = app;
